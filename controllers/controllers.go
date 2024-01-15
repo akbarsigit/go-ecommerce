d7a160708cd6dab8ea3c7ce0fac204b327fab33f
Akbar Sigit P <akbarsigit3@gmail.com>
2024-01-15 19:54:03 +0700

    feat: adding function foundation

---
+++ controllers/controllers.go
@@ -0,0 +1,25 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func hashPassword(password string) string {
+
+}
+
+func VerifyPassword(userPassword string, givenPassword string) (bool, error) {
+
+}
+
+func Signup() gin.HandlerFunc {
+
+}
+
+func Login() gin.HandlerFunc {
+
+}
+
+func SearchProducts() gin.HandlerFunc {
+}
+
+func SearchProductByQuery() gin.HandlerFunc {
+}
